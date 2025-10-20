a9929601b4a73bcb968a86859d3f7bfdffc4cc26
AndresBerre23 <andres.berrezueta.est@tecazuay.edu.ec>
2025-10-20 17:38:37 -0500

    Comentarios guia

--- ejemploFacade/src/facade/VideoConversionFacade.java
+++ ejemploFacade/src/facade/VideoConversionFacade.java
@@ -6,22 +6,23 @@ import some_complex_media_library.*;//importar todo del paquete
 
 /**
  *
- * @author aberr
+ * En esta clase simplemente pareciera algo sencillo ya que los procesos largos se hacen en cada clase
  */
+
 public class VideoConversionFacade {
     public File convertVideo(String fileName, String format) {
         System.out.println("VideoConversionFacade: conversion started.");
-        VideoFile file = new VideoFile(fileName);
-        Codec sourceCodec = CodecFactory.extract(file);
+        VideoFile file = new VideoFile(fileName);// clase videofile
+        Codec sourceCodec = CodecFactory.extract(file);//clase Codec
         Codec destinationCodec;
         if (format.equals("mp4")) {
-            destinationCodec = new MPEG4CompressionCodec();
+            destinationCodec = new MPEG4CompressionCodec();//clase mpeg4CompressionCodec
         } else {
-            destinationCodec = new OggCompressionCodec();
+            destinationCodec = new OggCompressionCodec();//clase ogg
         }
-        VideoFile buffer = BitrateReader.read(file, sourceCodec);
+        VideoFile buffer = BitrateReader.read(file, sourceCodec);//clase bitrateReader
         VideoFile intermediateResult = BitrateReader.convert(buffer, destinationCodec);
-        File result = (new AudioMixer()).fix(intermediateResult);
+        File result = (new AudioMixer()).fix(intermediateResult);//Audiomixer
         System.out.println("VideoConversionFacade: conversion completed.");
         return result;
     }
